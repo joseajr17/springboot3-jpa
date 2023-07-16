494673d77832bd857033298102574c9ed108a66f
José <josejr017@gmail.com>
2023-07-16 12:32:05 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/joseantonio/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.joseantonio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joseantonio.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
